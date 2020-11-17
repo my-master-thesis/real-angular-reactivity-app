d497f88ca41683fbd36b769eac57365876343de2
Marko Lavrinec <marko.lavrinec@specto.si>
2020-11-17 10:33:47 +0100

    test fix

--- src/app/boundary-cases/test/test.component.ts
+++ src/app/boundary-cases/test/test.component.ts
@@ -70,7 +70,7 @@ export class TestComponent implements OnInit {
   clickTimeout() {
     console.time('Function timeout increase');
     const tmp = Date.now();
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 10000; i++) {
       setTimeout(() => {
         this.counterC++;
       }, 1);
